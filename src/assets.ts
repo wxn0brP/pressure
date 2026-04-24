9db53684443b279d913912b846d914051e22fa89
wxn0brP <toja.filiper@gmail.com>
2026-04-24 20:19:44 +0200

    fix: bugs

--- src/assets.ts
+++ src/assets.ts
@@ -6,9 +6,6 @@ export async function copyAssets(config: Config) {
     if (!assets.length) return console.log("[PRESSURE] No assets found");
 
     for (const file of assets) {
-        if (process.platform === "win32")
-            await $`copy ${file} dist`;
-        else
-            await $`cp ${file} dist`;
+        await $`${process.platform === "win32" ? "copy" : "cp"} ${file} ${config.dir}`;
     }
 }
